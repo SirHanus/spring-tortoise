2e933dd2e39f96fdfd4200fa0b014711462a52f6
Jakub Hanuška <personal@jakubhanuska.cz>
2024-03-07 16:09:26 +0100

    Updated to seminar 2 code

--- src/main/java/cz/mendelu/ea/seminar1/utils/exceptions/InsufficientFunds.java
+++
@@ -1,9 +0,0 @@
-package cz.mendelu.ea.seminar1.utils.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.CONFLICT)
-public class InsufficientFunds extends RuntimeException {
-
-}
