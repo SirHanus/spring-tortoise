0ce110dcd2d39fd1b148a5829cba1251a84c36e3
Jakub Hanuška <personal@jakubhanuska.cz>
2024-03-26 18:27:47 +0100

    Parametrized users

--- src/test/java/cz/mendelu/ea/domain/statistics/StatisticsServiceUnitTest.java
+++ src/test/java/cz/mendelu/ea/domain/statistics/StatisticsServiceUnitTest.java
@@ -2,8 +2,13 @@ package cz.mendelu.ea.domain.statistics;
 
 import cz.mendelu.ea.domain.user.User;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -11,14 +16,28 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StatisticsServiceUnitTest {
 
-    @Test
-    public void testGetNamesOfUsers() {
+    @ParameterizedTest
+    @MethodSource("testGetNamesOfUsersParameters")
+    public void testGetNamesOfUsers(List<User> users, List<String> expectedNames) {
         StatisticsService service = new StatisticsService(null, null, null);
 
 
-        User user1 = new User("Ivo", "Ivo");
-        User user2 = new User("Kuba", "Kuba");
-        List<String> results = service.getNamesOfUsers(List.of(user1, user2));
-        assertThat(results, containsInAnyOrder("Ivo", "Kuba"));
+        List<String> results = service.getNamesOfUsers(users);
+        assertThat(results, containsInAnyOrder(expectedNames.toArray()));
+    }
+
+    private static Stream<Arguments> testGetNamesOfUsersParameters() {
+        return Stream.of(
+                Arguments.of(List.of(
+                        new User("Ivo", "Ivo"),
+                        new User("Kuba", "Kuba")), List.of("Ivo", "Kuba")),
+                Arguments.of(
+                        List.of(
+                                new User("Petr", "Ivo"),
+                                new User("Honza", "Kuba")), List.of("Petr", "Honza")),
+                Arguments.of(
+                        List.of()
+                        , List.of())
+        );
     }
 }
