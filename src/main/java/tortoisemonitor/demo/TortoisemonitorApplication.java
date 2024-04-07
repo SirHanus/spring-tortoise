4e91ade4a6e9a100df75346583426a4d52d85004
Jakub Hanuška <personal@jakubhanuska.cz>
2024-04-07 14:11:07 +0200

    Init

---
+++ src/main/java/tortoisemonitor/demo/TortoisemonitorApplication.java
@@ -0,0 +1,13 @@
+package tortoisemonitor.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TortoisemonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TortoisemonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tortoisemonitor/demo/TortoisemonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package tortoisemonitor.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TortoisemonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
